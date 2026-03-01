43b06c137593e03b73b7806bbba3eab4c0c16b39
Maverique-jp <166344836+Maverique-jp@users.noreply.github.com>
2026-03-01 10:19:03 +0900

    開発環境の構築

---
+++ backend-service/src/main/java/com/eventpick/backend/EventpickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eventpick.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventpickBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventpickBackendApplication.class, args);
+	}
+
+}
---
+++ backend-service/src/test/java/com/eventpick/backend/EventpickBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventpick.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventpickBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
